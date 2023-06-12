806f1fdb99b52b7c86e7ac2f10ff7424211421ec
Xujuan Chen <xchen022@citymail.cuny.edu>
2023-06-12 18:02:35 -0400

    READ.ME & GIF

--- src/config/supabseClient.js
+++ src/config/supabseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_URL;
-const supabaseAPIKey = process.env.REACT_API_KEY;
+const supabaseUrl = process.env.REACT_APP_URL;
+const supabaseAPIKey = process.env.REACT_APP_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseAPIKey);
 
 export default supabase;
